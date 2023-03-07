9e3a3594c0b5faa56675df51a3ce43392407bdcf
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 22:35:44 +0800

    fix: table header name, column md 12

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
                     @if (session('status'))
@@ -21,23 +21,23 @@
                 <div class="card-body">
                     <table class="table">
                         <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                          </tr>
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">To do</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
                         </thead>
                         <tbody>
-                          <tr>
+                        <tr>
                             <th scope="row">1</th>
                             <td>Mark</td>
                             <td>Otto</td>
                             <td>@mdo</td>
-                          </tr>
+                        </tr>
 
                         </tbody>
-                      </table>
+                    </table>
                 </div>
             </div>
         </div>
