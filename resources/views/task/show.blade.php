eedc9b6511e8fddec8db4beaebc8241c7c727fb8
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 02:08:04 +0800

    add: show error if name and description fields are not filled in to show blade

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -14,16 +14,26 @@
                             <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ $task->name }}">
+                                <input id="name" type="text" class="form-control" name="name">
                             </div>
+                            @if ($errors->has('name'))
+                                <div class="col-md-10 mt-2" style="text-align: right">
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row mb-3">
                             <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
 
                             <div class="col-md-6">
-                                <textarea name="description" class="form-control" id="description" rows="2">{{ $task->description }}</textarea>
+                                <textarea name="description" class="form-control" id="description" rows="2"></textarea>
                             </div>
+                            @if ($errors->has('description'))
+                                <div class="col-md-10 mt-2" style="text-align: right">
+                                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row mb-0" style="text-align: right">
