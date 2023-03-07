c21071ab1a7bfe8201e79ee2af3c823cb66bd0b9
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 01:48:49 +0800

    add: display error if input fields are empty

--- resources/views/task/create.blade.php
+++ resources/views/task/create.blade.php
@@ -13,10 +13,15 @@
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-
+                        
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name">
                             </div>
+                            @if ($errors->has('name'))
+                                <div class="col-md-10 mt-2" style="text-align: right">
+                                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row mb-3">
@@ -25,6 +30,11 @@
                             <div class="col-md-6">
                                 <textarea name="description" class="form-control" id="description" rows="2"></textarea>
                             </div>
+                            @if ($errors->has('description'))
+                                <div class="col-md-10 mt-2" style="text-align: right">
+                                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                                </div>
+                            @endif
                         </div>
 
                         <div class="row mb-0" style="text-align: right">
