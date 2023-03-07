e15c08941315b66401f8fe626f38ba131b859e4a
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 01:48:16 +0800

    add: rules for name and description with messages to storetaskrequest, set authorize to true

--- app/Http/Requests/StoreTaskRequest.php
+++ app/Http/Requests/StoreTaskRequest.php
@@ -11,7 +11,7 @@ class StoreTaskRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class StoreTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'description' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '**Please fill in name**',
+            'description.required' => '**Please fill in description**'
         ];
     }
 }
