4158efe6f8780ec30b7d39b1db0253d01cce2730
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 23:01:29 +0800

    add: create blade to task folder

---
+++ resources/views/task/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Add New Task</div>
+
+                <div class="card-body">
+                    <form method="POST" action="">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
+
+                            <div class="col-md-6">
+                                <textarea name="description" class="form-control" id="description" rows="2"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="row mb-0" style="text-align: right">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                Create
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
