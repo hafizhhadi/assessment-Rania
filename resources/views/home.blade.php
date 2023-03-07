a08f2d3b02ca11243fccdbb452611974a97bc9a1
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 23:59:47 +0800

    add: foreach to home blade show tasks fields

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,21 +25,22 @@
                     <br>
                     <table class="table">
                         <thead>
-                        <tr>
-                            <th scope="col">No.</th>
-                            <th scope="col">To do</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Action</th>
-                        </tr>
+                            <tr>
+                                <th>No.</th>
+                                <th>To do</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-
+                            @foreach ($tasks as $keys => $task)
+                                <tr>
+                                    <td>{{ $keys + 1 }}</td>
+                                    <td>{{ $task->name }}</td>
+                                    <td>{{ $task->description }}</td>
+                                    <td>-</td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
