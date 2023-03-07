af90ad54380a18e3424da51dc41f315b01312407
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 23:59:17 +0800

    add: tasks, compact tasks to index function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tasks = Task::all();
+        
+        return view('home', compact('tasks'));
     }
 }
