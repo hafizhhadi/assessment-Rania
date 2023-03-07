1e9e4f7315ab33a6ca5f2cbc75b213690c99603f
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 22:56:38 +0800

    add: card header title, add input type name

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,8 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
+                <div class="card-header">Add New Task</div>
+
                 <div class="card-body">
-                  test
+                    <div class="row mb-3">
+                        <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+                        <div class="col-md-6">
+                            <input id="name" type="text" class="form-control" name="name">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
