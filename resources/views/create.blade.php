60a7c2fa628cdd69291e692ba79b47bed74c66e6
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-07 22:57:13 +0800

    add: input type text area to create blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,6 +15,14 @@
                             <input id="name" type="text" class="form-control" name="name">
                         </div>
                     </div>
+
+                    <div class="row mb-3">
+                        <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
+
+                        <div class="col-md-6">
+                            <textarea name="description" class="form-control" id="description" rows="2"></textarea>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
