f9bef60b4d949551b6ef564e3ff50ea7c1e4c6a8
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 00:37:33 +0800

    add: form method post, action route task update passing task

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -8,30 +8,32 @@
                 <div class="card-header">Edit New Task</div>
 
                 <div class="card-body">
+                    <form method="POST" action="{{ route('task.update', $task) }}">
+                        @csrf
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
 
-                    <div class="row mb-3">
-                        <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-
-                        <div class="col-md-6">
-                            <input id="name" type="text" class="form-control" name="name" value="{{ $task->name }}">
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ $task->name }}">
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row mb-3">
-                        <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
+                        <div class="row mb-3">
+                            <label for="description" class="col-md-4 col-form-label text-md-end">Description</label>
 
-                        <div class="col-md-6">
-                            <textarea name="description" class="form-control" id="description" rows="2">{{ $task->description }}</textarea>
+                            <div class="col-md-6">
+                                <textarea name="description" class="form-control" id="description" rows="2">{{ $task->description }}</textarea>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="row mb-0" style="text-align: right">
-                        <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-outline-success btn-sm">
-                            Save
-                            </button>
+                        <div class="row mb-0" style="text-align: right">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-outline-success btn-sm">
+                                Save
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
