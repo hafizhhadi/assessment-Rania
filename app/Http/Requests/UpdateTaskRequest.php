bf1c509a20ec1d8825aa2dfccece082722679ca7
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 02:06:06 +0800

    add: rules and messages to name and description in updatetaskrequest

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -8,13 +8,22 @@ class UpdateTaskRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'description' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => '**Please fill in name**',
+            'description.required' => '**Please fill in description**',
         ];
     }
 }
