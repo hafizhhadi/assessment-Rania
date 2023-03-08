3e59bf7f671e5ef4b02b95695703a21741967e56
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 13:02:57 +0800

    add: modal for update blade

---
+++ resources/views/task/modal/update.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="updateModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="updateModalLabel">Update</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Confirm Update Task?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-outline-success btn-sm">Save</button>
+        </div>
+      </div>
+    </div>
+</div>
