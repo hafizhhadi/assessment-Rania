a50032061c31f0502fc156ec2cb5ea10d4c3eb91
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 14:38:49 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 //Task
 Route::get('/create', [TaskController::class, 'create'])->name('task.create');
 Route::post('/store', [TaskController::class, 'store'])->name('task.store');
-Route::get('/show/{task}', [TaskController::class, 'show'])->name('task.show');
-Route::post('/update/{task}', [TaskController::class, 'update'])->name('task.update');
-Route::get('/delete/{task}', [TaskController::class, 'delete'])->name('task.delete');
-Route::get('/complete/{task}', [TaskController::class, 'completeTask'])->name('task.complete');
+Route::get('/show/{task:uuid}', [TaskController::class, 'show'])->name('task.show');
+Route::post('/update/{task:uuid}', [TaskController::class, 'update'])->name('task.update');
+Route::get('/delete/{task:uuid}', [TaskController::class, 'delete'])->name('task.delete');
+Route::get('/complete/{task:uuid}', [TaskController::class, 'completeTask'])->name('task.complete');
