2c64eaeb0aa2cf80522bb7ec661d9a1439c4beae
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 15:05:35 +0800

    add: bootstrap link task to table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,19 +55,7 @@
                     <br>
                     <nav aria-label="Page navigation example">
                         <ul class="pagination pagination-sm justify-content-center">
-                          <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Previous">
-                              <span aria-hidden="true">&laquo;</span>
-                            </a>
-                          </li>
-                          <li class="page-item"><a class="page-link" href="#">1</a></li>
-                          <li class="page-item"><a class="page-link" href="#">2</a></li>
-                          <li class="page-item"><a class="page-link" href="#">3</a></li>
-                          <li class="page-item">
-                            <a class="page-link" href="#" aria-label="Next">
-                              <span aria-hidden="true">&raquo;</span>
-                            </a>
-                          </li>
+                            {{ $tasks->links() }}
                         </ul>
                     </nav>
                 </div>
