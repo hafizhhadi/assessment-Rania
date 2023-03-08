4ec442de120d578171ba15e19125ca461ace5dfb
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 22:26:25 +0800

    add: accordian items, add count

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
                     @if (session('status'))
@@ -25,8 +25,6 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    <div> Total Finish Tasks: {{ $completedTasks }} </div>
-                    <div> Total In-Progress Tasks: {{ $inProgressTasks }} </div>
                     <div style="text-align: right">
                         <a href="{{ route('task.create') }}" type="button" class="btn btn-outline-primary">Add Task</a>
                     </div>
@@ -52,7 +50,7 @@
                                     <td>
                                         <button type="button" class="btn btn-outline-success btn-sm" data-bs-toggle="modal" data-bs-target="#doneModal">Done</button>
                                         <a href="{{ route('task.show', $task) }}" type="button" class="btn btn-outline-warning btn-sm">Edit</a>
-                                        <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal">Delete</button>
+                                        <button type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $task->id }}">Delete</button>
                                     </td>
                                 </tr>
                                 @include('task.modal.done')
@@ -73,6 +71,42 @@
                             {{ $tasks->links() }}
                         </ul>
                     </nav>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#finishTask" aria-expanded="true" aria-controls="finishTask">
+                        Finish Tasks - {{ $completedTasks->count() }} Tasks
+                    </button>
+                  </h2>
+                  <div id="finishTask" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        @forelse ($completedTasks as $completedTask)
+                            <ul> <li>{{ ucfirst($completedTask->name) }}</li> </ul>
+                        @empty
+                            No Finish tasks
+                        @endforelse
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#inProgressTask" aria-expanded="false" aria-controls="inProgressTask">
+                        In-progress Tasks - {{ $inProgressTasks->count() }} Tasks
+                    </button>
+                  </h2>
+                  <div id="inProgressTask" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        @forelse ($inProgressTasks as $inProgressTask)
+                            <ul> <li>{{ ucfirst($inProgressTask->name) }}</li> </ul>
+                        @empty
+                            No In-progress tasks
+                        @endforelse
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
