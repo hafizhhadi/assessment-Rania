fbbe1b2816c609d4aceacafa6a0ffb829aef04fb
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 12:27:48 +0800

    fix: title and content  for modal finish, button size and colour

--- resources/views/task/modal/done.blade.php
+++ resources/views/task/modal/done.blade.php
@@ -2,15 +2,15 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="doneModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="doneModalLabel">Finish</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          Finish this task?
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-outline-success btn-sm">Done</button>
         </div>
       </div>
     </div>
