5535369c8119759a5b5178d11de6c67d67d54681
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 12:27:35 +0800

    fix: title and content  for modal delete, button size and colour

--- resources/views/task/modal/delete.blade.php
+++ resources/views/task/modal/delete.blade.php
@@ -2,15 +2,15 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure you want to delete?</h1>
+          <h1 class="modal-title fs-5" id="deleteModalLabel">Delete</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          Delete this task?
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-outline-danger btn-sm">Delete</button>
         </div>
       </div>
     </div>
