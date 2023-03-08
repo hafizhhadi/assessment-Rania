4c7382b044867895e7935c0a08e78e2ad40e48e7
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 12:49:46 +0800

    add: add modal blade for add task

---
+++ resources/views/task/modal/add.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="saveModal" tabindex="-1" aria-labelledby="saveModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="saveModalLabel">Finish</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Confirm Create Task?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-outline-success btn-sm">Confirm</button>
+        </div>
+      </div>
+    </div>
+</div>
