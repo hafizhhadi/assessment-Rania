49d7aaaaac0dde9826f8a7733f4e287a617b537a
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 21:37:29 +0800

    add: localization for messages

---
+++ lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'success' => 'New task has been added!',
+	'delete' => 'The task was successfully deleted!',
+    'update' => 'The task has been successfully updated!',
+    'complete' => 'The task has been completed!',
+];
