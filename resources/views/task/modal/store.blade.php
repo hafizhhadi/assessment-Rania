1e279d3a605b75611bacc768673baa184e434e97
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-08 12:55:56 +0800

    fix: naming for modal

--- resources/views/task/modal/store.blade.php
+++ resources/views/task/modal/store.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="saveModal" tabindex="-1" aria-labelledby="saveModalLabel" aria-hidden="true">
+<div class="modal fade" id="storeModal" tabindex="-1" aria-labelledby="storeModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="saveModalLabel">Finish</h1>
+          <h1 class="modal-title fs-5" id="storeModalLabel">Finish</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
