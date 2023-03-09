ad445eee060cc49f243e18c368779756695df452
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 18:01:21 +0800

    add: input field for status and created at

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -20,6 +20,18 @@
                         <textarea name="description" class="form-control-plaintext" id="description" readonly>{{ $task->description }}</textarea>
                     </div>
                 </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Status</label>
+                    <div class="col-sm-9">
+                        <input id="status" type="text" class="form-control-plaintext" name="status" value="{{ $task->status }}" readonly>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-3 col-form-label">Created At</label>
+                    <div class="col-sm-9">
+                        <input id="created_at" type="text" class="form-control-plaintext" name="created_at" value="{{ $task->getTaskCreatedAtDiffForHuman() }}" readonly>
+                    </div>
+                </div>
                 <div class="row" style="text-align: right">
                     <div>
                         <a href="{{ route('home') }}" type="button" class="btn btn-outline-dark btn-xs"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
