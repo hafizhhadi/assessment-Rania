3a5678984eb068918cf2fd366c519e72955cefd7
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 14:08:42 +0800

    fix: template for login blade, include layouts auth, section content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,87 +1,72 @@
-<!DOCTYPE html>
-<html lang="en" class="h-100">
 
-<head>
-	<title>To-do-list</title>
-	
-	<link rel="shortcut icon" type="image/png" href="images/favicon.png" />
-    <link href="{!! asset('/css/style.css') !!}" rel="stylesheet">
+@extends('layouts.auth')
 
-</head>
+@section('content')
+<div class="row justify-content-center h-100 align-items-center">
+    <div class="col-md-6">
+        <div class="authincation-content">
+            <div class="row no-gutters">
+                <div class="col-xl-12">
+                    <div class="auth-form">
+                        <div class="text-center mb-3">
+                            <a href="index.html"><img src="{!! asset('images/logo-full.png') !!}" alt=""></a>
+                        </div>
+                        <h4 class="text-center mb-4">Sign in your account</h4>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="mb-3">
+                                <label class="mb-1"><strong>Email</strong></label>
 
-<body class="vh-100">
-    <div class="authincation h-100">
-        <div class="container h-100">
-            <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-md-6">
-                    <div class="authincation-content">
-                        <div class="row no-gutters">
-                            <div class="col-xl-12">
-                                <div class="auth-form">
-									<div class="text-center mb-3">
-										<a href="index.html"><img src="images/logo-full.png" alt=""></a>
-									</div>
-                                    <h4 class="text-center mb-4">Sign in your account</h4>
-                                    <form method="POST" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="mb-3">
-                                            <label class="mb-1"><strong>Email</strong></label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="mail@mail.com" required autocomplete="email" autofocus>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="mail@mail.com" required autocomplete="email" autofocus>
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
+                            <div class="mb-3">
+                                <label class="mb-1"><strong>Password</strong></label>
 
-                                        <div class="mb-3">
-                                            <label class="mb-1"><strong>Password</strong></label>
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
 
-                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="password" required autocomplete="current-password">
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
+                            <div class="row d-flex justify-content-between mt-4 mb-2">
+                                <div class="mb-3">
+                                    <div class="form-check custom-checkbox ms-1">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                        <div class="row d-flex justify-content-between mt-4 mb-2">
-                                            <div class="mb-3">
-                                               <div class="form-check custom-checkbox ms-1">
-                                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-													<label class="form-check-label" for="basic_checkbox_1">Remember my preference</label>
-												</div>
-                                            </div>
-                                            <div class="mb-3">
-                                                @if (Route::has('password.request'))
-                                                    <a href="{{ route('password.request') }}">Forgot Password?</a>
-                                                @endif
-                                            </div>
-                                        </div>
-
-                                        <div class="text-center">
-                                            <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
-                                        </div>
-                                    </form>
-
-                                    <div class="new-account mt-3">
-                                        <p>Don't have an account? <a class="text-primary" href="{{ route('register') }}">Sign up</a></p>
+                                        <label class="form-check-label" for="basic_checkbox_1">Remember my preference</label>
                                     </div>
                                 </div>
+                                <div class="mb-3">
+                                    @if (Route::has('password.request'))
+                                        <a href="{{ route('password.request') }}">Forgot Password?</a>
+                                    @endif
+                                </div>
                             </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+                            </div>
+                        </form>
+
+                        <div class="new-account mt-3">
+                            <p>Don't have an account? <a class="text-primary" href="{{ route('register') }}">Sign up</a></p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+@endsection
 
-    <script src="{!! asset('vendor/global/global.min.js') !!}"></script>
-    <script src="{!! asset('js/custom.min.js') !!}"></script>
-    <script src="{!! asset('/js/dlabnav-init.js') !!}"></script>
-	
-</body>
-</html>
+
+
 
