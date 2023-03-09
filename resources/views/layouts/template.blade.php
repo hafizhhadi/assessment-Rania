e64ba7d45f9180ef55e778efb95841517218bf96
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 21:31:50 +0800

    rmv: unused script and css template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -19,7 +19,6 @@
 	<!-- FAVICONS ICON -->
 	<link rel="shortcut icon" type="image/png" href="#" />
 	<link href="{!! asset('vendor/jquery-nice-select/css/nice-select.css') !!}" rel="stylesheet">
-	<link href="{!! asset('vendor/owl-carousel/owl.carousel.css') !!}" rel="stylesheet">
 	
 	<!-- Style css -->
     <link href="{!! asset('css/style.css') !!}" rel="stylesheet">
@@ -96,70 +95,12 @@
     ***********************************-->
     <!-- Required vendors -->
     <script src="{!! asset('vendor/global/global.min.js') !!}"></script>
-	<script src="{!! asset('vendor/chart.js/Chart.bundle.min.js') !!}"></script>
 	<script src="{!! asset('vendor/jquery-nice-select/js/jquery.nice-select.min.js') !!}"></script>
-	
-	<!-- Apex Chart -->
-	<script src="{!! asset('vendor/apexchart/apexchart.js') !!}"></script>
-	
-	<script src="{!! asset('vendor/chart.js/Chart.bundle.min.js') !!}"></script>
-	
-	<!-- Chart piety plugin files -->
     <script src="{!! asset('vendor/peity/jquery.peity.min.js') !!}"></script>
-	
-	<!-- Dashboard 1 -->
-	<script src="{!! asset('js/dashboard/dashboard-1.js') !!}"></script>
-	
 	<script src="{!! asset('vendor/owl-carousel/owl.carousel.js') !!}"></script>
-	
     <script src="{!! asset('js/custom.min.js') !!}"></script>
 	<script src="{!! asset('js/dlabnav-init.js') !!}"></script>
 	
     
-	<script>
-		function JobickCarousel()
-			{
-
-				/*  testimonial one function by = owl.carousel.js */
-				jQuery('.front-view-slider').owlCarousel({
-					loop:false,
-					margin:30,
-					nav:true,
-					autoplaySpeed: 3000,
-					navSpeed: 3000,
-					autoWidth:true,
-					paginationSpeed: 3000,
-					slideSpeed: 3000,
-					smartSpeed: 3000,
-					autoplay: false,
-					animateOut: 'fadeOut',
-					dots:true,
-					navText: ['', ''],
-					responsive:{
-						0:{
-							items:1
-						},
-						
-						480:{
-							items:1
-						},
-						
-						767:{
-							items:3
-						},
-						1750:{
-							items:3
-						}
-					}
-				})
-			}
-
-			jQuery(window).on('load',function(){
-				setTimeout(function(){
-					JobickCarousel();
-				}, 1000);
-			});
-	</script>
-
 </body>
 </html>
