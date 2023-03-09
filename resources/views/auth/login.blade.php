52754205973b4285d76608777076509910ffee99
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 14:43:46 +0800

    fix: route to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
                 <div class="col-xl-12">
                     <div class="auth-form">
                         <div class="text-center mb-3">
-                            <a href="#"><img src="{!! asset('images/logo-full.png') !!}" alt=""></a>
+                            <a href="{{ route('login') }}"><img src="{!! asset('images/logo-full.png') !!}" alt=""></a>
                         </div>
                         <h4 class="text-center mb-4">Sign in your account</h4>
                         <form method="POST" action="{{ route('login') }}">
