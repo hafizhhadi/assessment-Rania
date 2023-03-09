27a18864a5e8c5bd269f30a9342d76dc68d64e94
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 14:08:21 +0800

    add: template for auth

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Jobick : Job Admin Bootstrap 5 Template" />
+	<meta property="og:title" content="Jobick : Job Admin Bootstrap 5 Template" />
+	<meta property="og:description" content="Jobick : Job Admin Bootstrap 5 Template" />
+	<meta property="og:image" content="https://jobick.dexignlab.com/xhtml/social-image.png" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Jobick Job Admin</title>
+	
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="{!! asset('images/favicon.png') !!}" />
+    <link href="{!! asset('css/style.css') !!}" rel="stylesheet">
+
+</head>
+
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            @yield('content')
+        </div>
+    </div>
+
+
+    <!--**********************************
+        Scripts
+    ***********************************-->
+    <!-- Required vendors -->
+    <script src="{!! asset('vendor/global/global.min.js') !!}"></script>
+    <script src="{!! asset('js/custom.min.js') !!}"></script>
+    <script src="{!! asset('js/dlabnav-init.js') !!}"></script>
+	
+</body>
+</html>
