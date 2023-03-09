7ddd12e9cc73ecfed4f23bf34c60b97dc55a797c
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-10 02:15:48 +0800

    fix: user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
         User::create([
-            'name' => 'user',
-            'email' => 'user@user.com',
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
             'password' => bcrypt('password'),
         ]);
     }
