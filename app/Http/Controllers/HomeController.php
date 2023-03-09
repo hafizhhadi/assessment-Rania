5552a9b7e96c0788a8510fe79f74fafe01a45ef1
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 19:02:16 +0800

    rmv: comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,21 +8,11 @@ use App\Enums\TaskStatusEnum;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         $tasks = Task::ByUser(auth()->user()->id)->latest()->paginate(5);
