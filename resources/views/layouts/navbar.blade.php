12928c21b4646881cc706685daf997e4fbc7f2bf
Hafiz Hadi <hafizhadi581@gmail.com>
2023-03-09 12:14:20 +0800

    add: template for navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand">
+    <div class="collapse navbar-collapse justify-content-between">
+        <div class="header-left">
+            <div class="dashboard_bar">
+                Dashboard
+            </div>
+        </div>
+    </div>
+</nav>
